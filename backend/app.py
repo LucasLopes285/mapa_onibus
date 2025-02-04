d8f749128264bb23c97ebbb9b6337e6c71ba2303
Lucas Lopes <lucas.haddad15@gmail>
2025-02-04 07:23:48 -0300

    Atualizando app.py para incluir a conexão com o banco de dados

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routes.rotas import router as rotas_router
 from routes.paradas import router as paradas_router
 from routes.usuarios import router as usuarios_router
+from database import engine
 from models import Base
 
 app = FastAPI()
@@ -12,3 +13,6 @@ app.include_router(paradas_router, prefix="/api", tags=["Paradas"])
 app.include_router(usuarios_router, prefix="/api", tags=["Usuários"])
 
 
+@app.get("/")
+def home():
+    return {"mensagem": "API de Rotas de Ônibus funcionando!"}
